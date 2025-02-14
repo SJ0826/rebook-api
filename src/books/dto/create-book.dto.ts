6eae8956b183275013d2b1ad69bb8c72172dbc2f
SJ0826 <ikosdu60@gmail.com>
2025-02-14 23:55:41 +0900

    feat: book create, update dto 생성 (#5)

---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,19 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  author: string;
+
+  @IsString()
+  publisher: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+}
---
+++ src/books/dto/update-book.dto.ts
@@ -0,0 +1,23 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateBookDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  author?: string;
+
+  @IsOptional()
+  @IsString()
+  publisher?: string;
+
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+
+  @IsOptional()
+  @IsNumber()
+  description?: string;
+}
