534069a741662a1704b55a77ca0171f6a29e4e8d
SJ0826 <ikosdu60@gmail.com>
2025-07-20 14:23:46 +0900

    chore: github actions 연동

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'rebook-api', // 애플리케이션 이름
-      script: './dist/src/main.js', // 실행할 파일
+      script: './dist/main.js', // 실행할 파일
       instances: 1, // 싱글 인스턴스 실행 (다중 실행 시 cluster 모드 사용)
       autorestart: true, // 크래시 발생 시 자동 재시작
       watch: false, // 코드 변경 감지 비활성화 (개발 모드에서는 true 가능)
